bd92cd2377c65052290178b434c001511f4b75ac
りゅま <ryumayotoboshi@gmail.com>
2026-06-16 16:51:30 +0900

    notes.js を作成

---
+++ js/notes.js
@@ -0,0 +1,44 @@
+import { loadNotes, saveNotes } from "./storage.js";
+
+let notes = loadNotes();
+
+export function getNotes() {
+    return notes;
+}
+
+export function getNote(id) {
+
+    return notes.find(
+        note => note.id === id
+    );
+
+}
+
+export function addNote(title, content) {
+
+    const note = {
+        id: crypto.randomUUID(),
+        title,
+        content
+    };
+
+    notes.push(note);
+
+    saveNotes(notes);
+
+    return note;
+}
+
+export function updateNote(id, title, content) {
+
+    const note = getNote(id);
+
+    if (!note) {
+        return;
+    }
+
+    note.title = title;
+    note.content = content;
+
+    saveNotes(notes);
+}
